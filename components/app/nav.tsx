d0ace66f7671e2ff6d491cd3591d7f7a713408bc
Miguel Margodt <miguel.margodt@hotmail.com>
2026-03-04 14:39:44 +0100

    routes link

--- components/app/nav.tsx
+++ components/app/nav.tsx
@@ -4,7 +4,7 @@ import { Logo } from "./logo";
 export const Nav = () => {
   return (
     <nav className="bg-background max-w-7xl mx-auto px-4 md:px-8 py-4">
-      <div className="  flex justify-between">
+      <div className="flex items-center gap-6">
         <Link href="/">
           <div className="flex gap-2 items-center">
             <Logo className="size-6 md:size-8" />
@@ -13,6 +13,14 @@ export const Nav = () => {
             </div>
           </div>
         </Link>
+        <div className="flex items-center gap-4 pt-[1px]">
+          <Link
+            href="/"
+            className="text-sm font-semibold text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Routes
+          </Link>
+        </div>
       </div>
     </nav>
   );
